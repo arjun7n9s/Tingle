df157766622a0463716e6f346b963438f66d3eb7
arjun7n9s <arjun7n9s@gmail.com>
2026-08-22 18:30:12 +0530

    Point Tingle jobs at http.ts and drop the unused Phase-3 shell files

--- packages/tingle-core/src/index.ts
+++ packages/tingle-core/src/index.ts
@@ -1,20 +1,38 @@
-export * from "./config.js";
-export * from "./collectors.js";
-export * from "./artifacts.js";
-export * from "./claim.js";
-export * from "./dates.js";
-export * from "./piles.js";
-export * from "./adjuncts.js";
-export * from "./store.js";
-export * from "./schema/hits.js";
-export * from "./schema/events.js";
-export * from "./schema/profile.js";
-export * from "./bd/client.js";
-export * from "./bd/validate.js";
-export * from "./bd/scrape.js";
-export * from "./bd/mock.js";
-export * from "./jobs/firstLook.js";
-export * from "./auth.js";
-export * from "./analyst.js";
-export * from "./views.js";
-export * from "./server.js";
+export { loadEnv, loadTingleConfig, triggerInputs, requirePinned, chaosBrokenUrl } from "./config.js";
+export type { TingleConfig, CollectorKey } from "./config.js";
+export {
+  BrightDataClient,
+  BrightDataError,
+  classifyHealStatus,
+} from "./bd/client.js";
+export type { HealProgress, TriggerInput } from "./bd/client.js";
+export { scrapeAndValidate } from "./bd/scrape.js";
+export type { ScrapeOutcome, ScrapeOpts } from "./bd/scrape.js";
+export { validateRows, buildHealPrompt, isValidationSuccess } from "./bd/validate.js";
+export { HitRowSchema, HitSourceSchema, normalizeRow, domainFromUrl } from "./schema/hits.js";
+export type { HitRow, HitSource } from "./schema/hits.js";
+export { HealEventSchema, TingleEventSchema } from "./schema/events.js";
+export type { HealEvent, TingleEvent } from "./schema/events.js";
+export { WatchProfileSchema } from "./schema/profile.js";
+export type { WatchProfile, Stage } from "./schema/profile.js";
+export {
+  proposeClaim,
+  buildFingerprints,
+  isClaimRelevant,
+  scoreAgainstClaim,
+} from "./claim.js";
+export { mapHitsToPiles, pileCounts, emptyPiles } from "./piles.js";
+export type { Piles, PileHit, PileKey } from "./piles.js";
+export { firstLook, parseFirstLookRequest, ANALYST_CONTRACT } from "./jobs/firstLook.js";
+export type {
+  FirstLookRequest,
+  FirstLookResult,
+  FirstLookNeedsConfirm,
+} from "./jobs/firstLook.js";
+export { tingleTick, TINGLE_TRANSPORT } from "./jobs/tingleTick.js";
+export type { TickResult, TickProject, TickOpts } from "./jobs/tingleTick.js";
+export { PAUSE_COPY } from "./budget.js";
+export { fileMailer } from "./mail.js";
+export { VAULT_PROMISE, seal, open } from "./vault.js";
+export { clusterHits, muteTokens, CLAIM_LOCK_WARNING } from "./dedup.js";
+export { fetchAdjuncts } from "./adjunct.js";
